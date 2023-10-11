4bfff0caeb5f3608712875f9c4c1523901684a18
Diwas10 <rimaldiwas10@gmail.com>
2023-10-11 19:03:47 +0545

    Edit drawing and pause on checkmate

--- src/chess.c
+++ src/chess.c
@@ -8,13 +8,13 @@ int main(void)
     Color background = {.r = 31, .g = 31, .b = 40, .a = 255};
     Board board = initBoard();
     PromotionWindow pwin = initPromotionWindow();
-    bool game_finished = false;
+    bool draw_debug_hints = false;
 
-    while (!WindowShouldClose() && !game_finished) {
+    while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(background);
 
-        if (IsMouseButtonPressed(0)) {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !board.checkmate) {
             if (board.promotion_pending)
                 handlePromotion(GetMouseX(), GetMouseY(), &board, pwin);
             else
@@ -31,39 +31,28 @@ int main(void)
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
                 Cell c = board.cells[y][x];
-
-                // char str[10];
-                // sprintf(str, "%d %d", y, x);
-                // DrawText(str, c.pos.x + 30, c.pos.y + 10, 20, PURPLE);
-
                 const char *s = getPieceTypeString(c.piece);
                 DrawRectangle(c.pos.x, c.pos.y, CELL_SIZE, CELL_SIZE, c.bg);
 
-                if (c.is_dangerous[white]) {
-                    DrawText("DW", c.pos.x + 40, c.pos.y + 10, 12, RED);
-                }
-                if (c.is_dangerous[black]) {
-                    DrawText("DB", c.pos.x + 40, c.pos.y + 30, 12, RED);
-                }
-
-                if (c.blocks_check) {
-                    DrawText("CBS", c.pos.x + 40, c.pos.y + 50, 10, BLACK);
-                }
-
-                if (c.opens_check) {
-                    DrawText("PIN", c.pos.x + 40, c.pos.y + 70, 10, BLACK);
+                if (draw_debug_hints) {
+                    if (c.is_dangerous[white])
+                        DrawText("DW", c.pos.x + 40, c.pos.y + 10, 12, RED);
+                    if (c.is_dangerous[black])
+                        DrawText("DB", c.pos.x + 40, c.pos.y + 30, 12, RED);
+                    if (c.blocks_check)
+                        DrawText("CBS", c.pos.x + 40, c.pos.y + 50, 10, BLACK);
+                    if (c.opens_check)
+                        DrawText("PIN", c.pos.x + 40, c.pos.y + 70, 10, BLACK);
                 }
 
                 if (!s)
                     continue;
                 DrawText(s, c.pos.x + 10, c.pos.y + 10, 20, (c.piece.color == black) ? BLACK : GREEN);
-
             }
         }
 
         if (board.promotion_pending) {
             enum PieceColor promoting_color = board.promoting_cell->piece.color;
-
             DrawRectangle(pwin.pos.x, pwin.pos.y, pwin.width, pwin.height, background);
             DrawText(pwin.text, pwin.pos.x + pwin.width / 2 - pwin.text_width / 2, pwin.pos.y + pwin.padding, pwin.text_height, RAYWHITE);
 
@@ -75,15 +64,12 @@ int main(void)
             }
         }
 
-        // if (board.checkmate) {
-        //     // game_finished = true;
-        //     const char *text = "Checkmate!";
-        //     int fontSize = 50;
-        //     int width = MeasureText(text, fontSize);
-        //     DrawText(text, BOARD_SIZE / 2 - width / 2, BOARD_SIZE / 2 - fontSize / 2, fontSize, RED);
-        //     EndDrawing();
-        //     WaitTime(5);
-        // }
+        if (board.checkmate) {
+            char *text = "Checkmate!";
+            int size = 50;
+            int width = MeasureText(text, size);
+            DrawText(text, BOARD_SIZE / 2 - width / 2, BOARD_SIZE / 2 - size / 2, size, RED);
+        }
 
         EndDrawing();
     }
