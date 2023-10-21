7af3c752355bd3e7e9ce0dd30d2adededd7080f2
Diwas10 <rimaldiwas10@gmail.com>
2023-10-21 23:17:50 +0545

    Change some colors

--- src/helpers.h
+++ src/helpers.h
@@ -6,16 +6,16 @@
 #define BOARD_PADDING           10
 #define BOARD_SIZE              (CELL_SIZE * 8)
 #define WINDOW_SIZE             (BOARD_SIZE + BOARD_PADDING * 2)
-#define COLOR_RED               (Color){0xff, 0x55, 0x55, 0xff}
+#define COLOR_RED               (Color){0xd7, 0x6c, 0x6c, 0xff}
 #define COLOR_BLACK             (Color){0x4E, 0x53, 0x56, 0xff}
 #define COLOR_GREEN             (Color){0x50, 0xfa, 0x7b, 0xff}
 #define COLOR_GREY              (Color){0xc7, 0xce, 0xd1, 0xff}
 #define COLOR_WHITE             WHITE
-#define COLOR_CELL_ACTIVE       (Color){0xd0, 0x87, 0x70, 0xff}
-#define COLOR_CELL_MOVABLE      (Color){0x81, 0xa1, 0xc1, 0xff}
+#define COLOR_CELL_ACTIVE       (Color){0xd7, 0xc8, 0x6c, 0xff}
+#define COLOR_CELL_MOVABLE      (Color){0x8e, 0xb7, 0xd6, 0xff}
 #define COLOR_CELL_CASTLING     (Color){0x5e, 0x81, 0xac, 0xff}
 #define COLOR_CELL_CAPTURABLE   COLOR_RED
-#define COLOR_MOVE_SRC          COLOR_CELL_ACTIVE
+#define COLOR_MOVE_SRC          (Color){0xcb, 0xdd, 0xaf, 0xff}
 #define COLOR_MOVE_DST          COLOR_MOVE_SRC
 #define COLOR_CHECKER_DARK      COLOR_GREY
 #define COLOR_CHECKER_LIGHT     COLOR_WHITE
